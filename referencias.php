9d5663a5f20e6d516899a7c78c324921dbbbd67a
leocezana <leonardo.manhagna@edu.ufes.br>
2023-08-02 10:56:26 -0300

    ref (completa)
    cap1 completo (tentar personalizar lista numerada)
    cap2 completo

--- referencias.php
+++ referencias.php
@@ -25,8 +25,35 @@ include("components/page-header.php");
 <p><a href=https://blog.abmes.org.br/o-futuro-da-educacao-superior-no-brasil-2>https://blog.abmes.org.br/o-futuro-da-educacao-superior-no-brasil-2/</a></p>
 
 
-<p><a href=></a></p>
+<p><a href=https://www.scielo.br/scielo.php?script=sci_arttext&pid=S1414-40772019000300569>https://www.scielo.br/scielo.php?script=sci_arttext&pid=S1414-40772019000300569</a></p>
+<p><a href=http://fundacaotelefonicavivo.org.br/educacao-do-seculo-xxi/ensino-a-distancia-cresce-no-brasil-e-facilita-acesso-a-educacao-superior>http://fundacaotelefonicavivo.org.br/educacao-do-seculo-xxi/ensino-a-distancia-cresce-no-brasil-e-facilita-acesso-a-educacao-superior/</a></p>
+<p><a href=http://fundacaotelefonicavivo.org.br/educacao-do-seculo-xxi/como-paises-estao-desenvolvendo-boas-praticas-na-educacao-a-distancia>http://fundacaotelefonicavivo.org.br/educacao-do-seculo-xxi/como-paises-estao-desenvolvendo-boas-praticas-na-educacao-a-distancia/</a></p>
+<p><a href=https://www.uol.com.br/tilt/reportagens-especiais/como-a-tecnologia-esta-moldando-o-futuro-da-educacao/#end-card>https://www.uol.com.br/tilt/reportagens-especiais/como-a-tecnologia-esta-moldando-o-futuro-da-educacao/#end-card</a></p>
+<p><a href=http://www2.eca.usp.br/moran/wp-content/uploads/2013/12/cenario.pdf>http://www2.eca.usp.br/moran/wp-content/uploads/2013/12/cenario.pdf</a></p>
+<p><a href=https://minhabiblioteca.com.br/tecnologia-na-educacao-impactos-dez-anos>https://minhabiblioteca.com.br/tecnologia-na-educacao-impactos-dez-anos/</a></p>
+<p><a href=https://minhabiblioteca.com.br/instituicao-de-ensino-superior-era-digital>https://minhabiblioteca.com.br/instituicao-de-ensino-superior-era-digital/</a></p>
+<p><a href=http://www2.fiescnet.com.br/web/uploads/recursos/d1dbf03635c1ad8ad3607190f17c9a19.pdf>http://www2.fiescnet.com.br/web/uploads/recursos/d1dbf03635c1ad8ad3607190f17c9a19.pdf</a></p>
+<p><a href=http://www.sudene.gov.br>http://www.sudene.gov.br</a></p>
+<p><a href=https://desafiosdaeducacao.grupoa.com.br/inteligencia-artificial-ensino-personalizado/#:~:text=De%20acordo%20com%20o%20estudo,chegar%20a%2050%25%20em%202030.>https://desafiosdaeducacao.grupoa.com.br/inteligencia-artificial-ensino-personalizado/#:~:text=De%20acordo%20com%20o%20estudo,chegar%20a%2050%25%20em%202030.</a></p>
+<p><a href=https://noticias.portaldaindustria.com.br/noticias/educacao/inteligencia-artificial-nas-escolas-estudo-aponta-ferramentas-que-serao-usadas-ate-2030>https://noticias.portaldaindustria.com.br/noticias/educacao/inteligencia-artificial-nas-escolas-estudo-aponta-ferramentas-que-serao-usadas-ate-2030/</a></p>
+<p><a href=https://sae.digital/tecnologia-na-educacao-tendencias>https://sae.digital/tecnologia-na-educacao-tendencias/</a></p>
+<p><a href=https://blogbrasil.westcon.com/quais-os-impactos-da-inteligencia-artificial-na-educacao>https://blogbrasil.westcon.com/quais-os-impactos-da-inteligencia-artificial-na-educacao</a></p>
+<p><a href=https://blogbrasil.comstor.com/6-beneficios-da-inteligencia-artificial-para-o-setor-educacional>https://blogbrasil.comstor.com/6-beneficios-da-inteligencia-artificial-para-o-setor-educacional</a></p>
+<p><a href=http://www.nce.ufrj.br/ginape/publicacoes/trabalhos/t_2002/t_2002_renato_aposo_e_francine_vaz/iaeducacao.htm>http://www.nce.ufrj.br/ginape/publicacoes/trabalhos/t_2002/t_2002_renato_aposo_e_francine_vaz/iaeducacao.htm</a></p>
+<p><a href=https://canaldoensino.com.br/blog/5-desafios-da-educacao-a-distancia-no-brasil>https://canaldoensino.com.br/blog/5-desafios-da-educacao-a-distancia-no-brasil</a></p>
+<p><a href=https://www.scielo.br/scielo.php?script=sci_arttext&pid=S0104-40362020000100156#:~:text=O%20sistema%20UAB%20%C3%A9%20uma,as%20diferentes%20regi%C3%B5es%20do%20Pa%C3%ADs%E2%80%9D.>https://www.scielo.br/scielo.php?script=sci_arttext&pid=S0104-40362020000100156#:~:text=O%20sistema%20UAB%20%C3%A9%20uma,as%20diferentes%20regi%C3%B5es%20do%20Pa%C3%ADs%E2%80%9D.</a></p>
 
+
+<p><a href=https://www.ecodebate.com.br/2017/03/14/importancia-da-educacao-ambiental-no-ambiente-escolar-artigo-de-lucelia-granja-de-mello>https://www.ecodebate.com.br/2017/03/14/importancia-da-educacao-ambiental-no-ambiente-escolar-artigo-de-lucelia-granja-de-mello/</a></p>
+<p><a href=https://repositorio.ufsc.br/bitstream/handle/123456789/181030/102_00073.pdf?sequence=1&isAllowed=y>https://repositorio.ufsc.br/bitstream/handle/123456789/181030/102_00073.pdf?sequence=1&isAllowed=y</a></p>
+<p><a href=http://portal.mec.gov.br/docman/setembro-2018-pdf/97041-apresentac-a-o-censo-superior-u-ltimo/file>http://portal.mec.gov.br/docman/setembro-2018-pdf/97041-apresentac-a-o-censo-superior-u-ltimo/file</a></p>
+<p><a href=https://pt.linkedin.com/pulse/16-tend%C3%AAncias-de-faith-popcorn-julio-cardoso>https://pt.linkedin.com/pulse/16-tend%C3%AAncias-de-faith-popcorn-julio-cardoso</a></p>
+<p><a href=https://www.ibfeduca.com.br/sp1/agenda/ensino-hibrido-do-conceito-as-praticas-2230>https://www.ibfeduca.com.br/sp1/agenda/ensino-hibrido-do-conceito-as-praticas-2230</a></p>
+<p><a href=https://portalseer.ufba.br/index.php/rebap/article/view/25795/15658>https://portalseer.ufba.br/index.php/rebap/article/view/25795/15658</a></p>
+<p><a href=http://esud2014.nute.ufsc.br/anais-esud2014/files/pdf/126484.pdf>http://esud2014.nute.ufsc.br/anais-esud2014/files/pdf/126484.pdf</a></p>
+<p><a href=http://www.avaliacao.cefetmg.br/o-que-e-a-avaliacao-institucional>http://www.avaliacao.cefetmg.br/o-que-e-a-avaliacao-institucional/</a></p>
+<p><a href=http://download.inep.gov.br/educacao_superior/censo_superior/documentos/2019/censo_da_educacao_superior_2018-notas_estatisticas.pdf>http://download.inep.gov.br/educacao_superior/censo_superior/documentos/2019/censo_da_educacao_superior_2018-notas_estatisticas.pdf</a></p>
+<p><a href=https://educapes.capes.gov.br/bitstream/capes/206261/2/ExecucaoRecurosExtraorcamentarios-v3.pdf>https://educapes.capes.gov.br/bitstream/capes/206261/2/ExecucaoRecurosExtraorcamentarios-v3.pdf</a></p>
 
 
 <?php include('components/page-footer.php'); ?>
