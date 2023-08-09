9b58296b46e1b85139fc136ce283bfded3298f7c
leocezana <leonardo.manhagna@edu.ufes.br>
2023-08-09 11:12:17 -0300

    teste de capas 3
    & apaguei os arquivos de seções que n estavam no weblivro (epígrafe, conclusão, etc)

--- components/sumario.php
+++ components/sumario.php
@@ -20,28 +20,6 @@
     </ul>
   </li>
 
-
-  <li><a href="conclusao.php">Conclusão</a></li>
   <li><a href="referencias.php">Referências</a></li>
 
-  <li class="capitulo"><a href="apendices.php"><span class="titulo">Apêndices</span></a>
-    <ul class="intertitulos">
-      <li><a href="apendices.php#apendiceA">Apêndice A</a></li>
-      <li><a href="apendices.php#apendiceB">Apêndice B</a></li>
-      <li><a href="apendices.php#apendiceC">Apêndice C</a></li>
-      <li><a href="apendices.php#apendiceD">Apêndice D</a></li>
-      <li><a href="apendices.php#apendiceE">Apêndice E</a></li>
-      <li><a href="apendices.php#apendiceF">Apêndice F</a></li>
-      <li><a href="apendices.php#apendiceG">Apêndice G</a></li>
-    </ul>
-  </li>
-  <li class="capitulo"><a href="listas.php"><span class="titulo">Listas de imagens, tabelas e quadros</span></a>
-    <ul class="intertitulos">
-      <li><a href="listas.php#lista-esquemas">Lista de Esquemas</a></li>
-      <li><a href="listas.php#lista-figuras">Lista de Figuras</a></li>
-      <li><a href="listas.php#lista-graficos">Lista de Gráficos</a></li>
-      <li><a href="listas.php#lista-quadros">Lista de Quadros</a></li>
-      <li><a href="listas.php#lista-tabelas">Lista de Tabelas</a></li>
-    </ul>
-  </li>
 </ul>
