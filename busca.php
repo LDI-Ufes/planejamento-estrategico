3b65581c92c9869a21f9651a789f7a10f10444cc
leocezana <leonardo.manhagna@edu.ufes.br>
2023-09-06 11:57:43 -0300

    updates z

--- busca.php
+++ busca.php
@@ -71,7 +71,8 @@
   <a id="voltar-topo" title="Voltar ao topo" href="#topo"><span>Voltar ao topo</span></a>
 
   <footer>
-    <span>2021 Sead Ufes. Todos os direitos reservados.</span>
+  <span>2021 - <?php echo date('Y'); ?> Sead Ufes. Todos os direitos reservados.</span>
+    <!-- <span>2021 Sead Ufes. Todos os direitos reservados.</span> -->
     <a href="https://ldi.ufes.br/" target="_blank" title="Abrir site do LDI em nova aba">Desenvolvido por LDI</a>
   </footer>
 
--- components/page-footer.php
+++ components/page-footer.php
@@ -10,11 +10,12 @@
 
   <a id="voltar-topo" title="Voltar ao topo" href="#topo"><span>Voltar ao topo</span></a>
   <footer>
-    <span>2022 Sead Ufes. Todos os direitos reservados.</span>
+    <!-- <span>2022 Sead Ufes. Todos os direitos reservados.</span> -->
+    <span>2021 - <?php echo date('Y'); ?> Sead Ufes. Todos os direitos reservados.</span>
     <a href="https://ldi.ufes.br/" target="_blank" title="Abrir site do LDI em nova aba">Desenvolvido por LDI</a>
   </footer>
 
-  <!-- <?php include 'components/lgpd-container.php'; ?> -->
+  <?php include 'components/lgpd-container.php'; ?>
   <script src="js/focus-visible.min.js"></script>
   <script type="text/javascript" src="js/app.min.js"></script>
   <script type="text/javascript" src="js/page.min.js"></script>
